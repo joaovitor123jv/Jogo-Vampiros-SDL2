2e22cd8111e890eb3d9f5b17923963abc618580a
João Vitor Antoniassi Segantin <jv.joaovitoras@gmail.com>
2016-10-12 22:14:59 -0300

    Defines em Animacao.h

--- src/Animacao/Animacao.h
+++ src/Animacao/Animacao.h
@@ -7,6 +7,18 @@
 
 #include<pthread.h> /* Para usar threads */
 
+#ifndef bool
+#define bool char
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
+
 typedef struct Animacao Animacao;
 
 /* CONSTRUTOR */
